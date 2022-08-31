a8c47cf2e5d47514508c2a5461724667ae4aae1c
gnvhswaroop <51911244+gnvhswaroop@users.noreply.github.com>
2022-08-31 18:04:42 +0530

    Add files via upload

---
+++ Prework1.java
@@ -0,0 +1,18 @@
+	import java.util.Scanner;
+class Main
+{
+    public static void main(String arg[])
+    {
+Scanner s= new Scanner(System.in);
+String s1;
+s1=s.nextLine();
+System.out.print(s1.charAt(0));
+for (int i=1;i<s1.length();i++)
+{
+if(s1.charAt(i)==' ')
+{
+System.out.print(s1.charAt(i+1));
+}
+}
+}
+}
